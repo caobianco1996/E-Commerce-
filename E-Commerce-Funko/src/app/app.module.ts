3bf354783ace7d14f05ff9ad7ae32b5a33b75656
caobianco1996 <caiocaobianco1@hotmail.com>
2022-09-06 17:55:16 -0300

    header

--- E-Commerce-Funko/src/app/app.module.ts
+++ E-Commerce-Funko/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import {CardModule} from 'primeng/card';
 import { AppRoutingModule } from './app-routing.module';
 import { NavigationModule } from './navigation/navigation.module';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    ButtonModule,
+    RadioButtonModule,
     CardModule,
     BrowserAnimationsModule,
     BrowserModule,
--- E-Commerce-Funko/src/app/navigation/navigation.module.ts
+++ E-Commerce-Funko/src/app/navigation/navigation.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {AccordionModule} from 'primeng/accordion';
+import {MenuItem} from 'primeng/api';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';   
 
 
 
@@ -11,7 +15,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    AccordionModule,
   ],
   exports: [
     HeaderComponent,
